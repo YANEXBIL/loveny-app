8fd2194e505963198b54bb908316263c2545dcf6
yanexbil <yanex77.ii@gmail.com>
2025-06-19 15:02:21 +0100

    Fix: Re-added 'widget_tweaks' to INSTALLED_APPS in settings.py

--- loveny_project/settings.py
+++ loveny_project/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts', # Your custom accounts app
+    'widget_tweaks', # <-- ADDED THIS LINE BACK
 ]
 
 MIDDLEWARE = [
